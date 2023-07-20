3ba3b479029e5a8dea2d0b146c7c7a3b3e5bc5e9
svelazquezmoby <svelazquez@mobydigital.com>
2023-07-20 11:44:15 -0300

    se agrego el dibujo libre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,134 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Draw from 'ol/interaction/Draw.js';
+import Map from 'ol/Map.js';
+import Polygon from 'ol/geom/Polygon.js';
+import View from 'ol/View.js';
+import { OSM, Vector as VectorSource } from 'ol/source.js';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer.js';
+import Select from 'ol/interaction/Select.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'openlayerdibu';
+export class AppComponent implements OnInit {
+  @ViewChild('mapElement', { static: true }) mapElement!: ElementRef;
+
+  private rasterLayer = new TileLayer({
+    source: new OSM(),
+  });
+
+  private vectorSource = new VectorSource({ wrapX: false });
+  private vectorLayer = new VectorLayer({
+    source: this.vectorSource,
+  });
+
+  private map!: Map;
+  private drawInteraction: Draw | null = null;
+  private isDrawing = false;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = new Map({
+      layers: [this.rasterLayer, this.vectorLayer],
+      target: this.mapElement.nativeElement,
+      view: new View({
+        center: [-11000000, 4600000],
+        zoom: 4,
+      }),
+    });
+  }
+
+  private addInteraction(typeShape: any): void {
+    let isFreehand: boolean = false;
+    if(typeShape=='LineString'){
+      isFreehand= true;
+    }
+    if (this.isDrawing && this.drawInteraction) {
+      this.map?.removeInteraction(this.drawInteraction);
+    }
+
+    if (typeShape !== 'None') {
+      if (typeShape === 'Square') {
+        this.drawInteraction = new Draw({
+          source: this.vectorSource,
+          type: 'Circle',
+          geometryFunction: createSquare,
+        });
+      } else {
+        this.drawInteraction = new Draw({
+          source: this.vectorSource,
+          type: typeShape,
+          freehand: isFreehand,
+        });
+      }
+      this.map?.addInteraction(this.drawInteraction);
+
+      const select = new Select();
+      this.drawInteraction.on('drawend', (event) => {
+        const geometry = event.feature.getGeometry();
+
+        select.getFeatures().clear();
+
+        if (geometry) {
+          this.vectorSource.forEachFeatureIntersectingExtent(
+            geometry.getExtent(),
+            (feature) => {
+              select.getFeatures().push(feature);
+            }
+          );
+        }
+      });
+
+      this.isDrawing = true;
+    } else {
+      this.isDrawing = false;
+    }
+  }
+
+  public onShapeButtonClick(typeShape: any): void {
+    console.log(typeShape)
+    this.addInteraction(typeShape);
+  }
 }
+
+// Función de geometría personalizada para crear un cuadrado a partir de un círculo.
+function createSquare(coordinates: any, geometry: any) {
+  const center = coordinates[0];
+  const last = coordinates[1];
+
+  const dx = center[0] - last[0];
+  const dy = center[1] - last[1];
+  const radius = Math.sqrt(dx * dx + dy * dy);
+
+  // Calculate the angle of rotation using Math.atan2()
+  const angle = Math.atan2(dy, dx);
+
+  // Number of sides in the square
+  const numSides = 4;
+
+  const newCoordinates = [];
+  for (let i = 0; i < numSides; i++) {
+    const currentAngle = angle + (i * 2 * Math.PI) / numSides;
+    const x = center[0] + radius * Math.cos(currentAngle);
+    const y = center[1] + radius * Math.sin(currentAngle);
+    newCoordinates.push([x, y]);
+  }
+
+  // Cerrar el cuadrado
+  newCoordinates.push(newCoordinates[0].slice());
+
+  if (!geometry) {
+    geometry = new Polygon([newCoordinates]);
+  } else {
+    geometry.setCoordinates([newCoordinates]);
+  }
+
+  return geometry;
+}
+
